623a29c7ec2a370466e956892009d4884276cac0
agore <agore@gannett.com>
2015-11-16 20:28:20 -0500

    various things

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -cp target/classes org.bitxbit.Main
+web: java -cp target/classes:target/dependency/* org.bitxbit.Main
--- src/main/java/org/bitxbit/Main.java
+++ src/main/java/org/bitxbit/Main.java
@@ -11,7 +11,7 @@ import java.util.Map;
  *
  */
 public class Main {
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://localhost:5050/myapp/";
 
 //    /**
 //     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
